ac3c03a1ebc46189bba59b397597ba6016866ec1
Yaming99 <32913524+Yaming99@users.noreply.github.com>
2020-06-11 05:12:21 +0200

    add tableau de joueurs

--- src/projet/EnsJoueurs.java
+++ src/projet/EnsJoueurs.java
@@ -1,39 +1,154 @@
 package projet;
 
-import java.io.Serializable;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.WindowConstants;
+import javax.swing.table.DefaultTableCellRenderer;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableCellRenderer;
+import javax.swing.table.TableColumn;
+import java.awt.Component;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.util.Vector;
 
+/**
+ * The type Ens joueurs.
+ */
 public class EnsJoueurs {
     private Vector<Joueur> vector = new Vector<Joueur>(20);
+    private String[] columnNames = {"N°", "Nom", "Score",
+            "Etat"};
+    private JTable table = new JTable() {
+        @Override
+        public Component prepareRenderer(TableCellRenderer renderer, int row, int column) {
+            // redimensione automatiquement la largeur des colonnes
+            Component component = super.prepareRenderer(renderer, row, column);
+            int rendererWidth = component.getPreferredSize().width;
+            TableColumn tableColumn = getColumnModel().getColumn(column);
+            tableColumn.setPreferredWidth(Math.max(rendererWidth + getIntercellSpacing().width, tableColumn.getPreferredWidth()));
+            return component;
+        }
+    };
+    private DefaultTableModel model = new DefaultTableModel();
+    private JScrollPane scrollPane = new JScrollPane(table);
+    private DefaultTableCellRenderer cellRenderer;
 
+    /**
+     * Creer.
+     *
+     * @param vector the vector
+     */
     public void creer(Vector<Joueur> vector) {
         this.vector = vector;
     }
 
+
+    /**
+     * Afficher.
+     */
     public void afficher() {
         System.out.println("Liste des joueurs :");
         vector.forEach(Joueur::afficher);
     }
 
+    /**
+     * Selectionner joueur joueur.
+     *
+     * @return the joueur
+     */
     public Joueur selectionnerJoueur() {
-
         int random = (int) (Math.random() * 100) % vector.size();
         return vector.get(random);
     }
 
+    /**
+     * Create window.
+     */
+    public void createWindow() {
+        JPanel panel = new JPanel();
+        panel.add(scrollPane);
+        // config fenetre
+        scrollPane.setBounds(20, 70, 1800, 300);
+        model.setColumnIdentifiers(columnNames); // nom des columns
+        table.setModel(model);
+        table.setFillsViewportHeight(true);
+        JFrame frame = new JFrame();
+        frame.setContentPane(panel);
+        frame.getContentPane().setLayout(null);
+        frame.setSize(500, 300);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+        frame.setTitle("Modifier un th\u00e8me");
+        table.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
+    }
+
+    /**
+     * Fill table.
+     */
+    public void fillTable() {
+        createWindow();
+        vector.forEach((player) -> model.addRow(new Object[]{player.getNumero(), player.getNom(), player.getScore(), player.getEtat()}));
+    }
+
+    /**
+     * Gets joueur.
+     *
+     * @param index the index
+     * @return the joueur
+     */
     public Joueur getJoueur(int index) {
         return vector.get(index);
     }
 
-    public int size(){
+    /**
+     * Size int.
+     *
+     * @return the int
+     */
+    public int size() {
         return vector.size();
     }
 
-    public void remove(Joueur ToRemove){
-        ToRemove.setEtat("\u00e9limin\u00e9");
-        vector.remove(ToRemove);
-    }
+    /**
+     * Gets vector.
+     *
+     * @return the vector
+     */
     public Vector<Joueur> getVector() {
         return vector;
     }
+
+    /**
+     * Remove.
+     *
+     * @param ToRemove the to remove
+     */
+    public void remove(Joueur ToRemove) {
+        ToRemove.setEtat("\u00e9limin\u00e9");
+        vector.remove(ToRemove);
+    }
+
+    /**
+     * Recup liste vector.
+     *
+     * @return the vector
+     */
+    public Vector<Joueur> recupListe() {
+        Vector<Joueur> joueurs = new Vector<>(20);
+        try {
+            File fichier = new File("src/projet/joueur/listeJoueurs.txt");
+            ObjectInputStream b = new ObjectInputStream(new FileInputStream(fichier));
+            joueurs = (Vector<Joueur>) b.readObject();
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return joueurs;
+    }
+
 }
--- src/projet/Joueur.java
+++ src/projet/Joueur.java
@@ -5,35 +5,69 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
 
+/**
+ * The type Joueur.
+ */
 public class Joueur implements Serializable {
-    private  int numero = 100;
-//    private char nom = 64; // '@' ascii
-    private  String nom;
+    private int numero = 100;
+    //    private char nom = 64; // '@' ascii
+    private String nom;
     private int score = 0;
     private String etat = "en attente";
     private ArrayList<String> listeEtats = new ArrayList<>(Arrays.asList("s\u00e9lectionn\u00e9", "gagnant", "super gagnant", "\u00e9limin\u00e9", "en attente"));
+    private static final long serialVersionUID = 8707119228364053775L;
 
+    /**
+     * Instantiates a new Joueur.
+     */
     public Joueur() {
         numero += 10;
     }
 
+    /**
+     * Sets nom.
+     *
+     * @param nom the nom
+     */
     public void setNom(String nom) {
         this.nom = nom;
     }
 
+    /**
+     * Gets nom.
+     *
+     * @return the nom
+     */
     public String getNom() {
         return nom;
     }
 
+    /**
+     * Instantiates a new Joueur.
+     *
+     * @param nomJ the nom j
+     * @param numJ the num j
+     */
     public Joueur(String nomJ, int numJ) {
         numero += numJ;
         nom = nomJ;
     }
 
+    /**
+     * Sets etat.
+     *
+     * @param etat the etat
+     */
     public void setEtat(String etat) {
         this.etat = etat;
     }
 
+    /**
+     * Saisir boolean.
+     *
+     * @param Question the question
+     * @return the boolean
+     */
     public boolean saisir(Question Question) {
         System.out.println("Saisir la r\u00e9ponse : ");
         Scanner sc = new Scanner(System.in);
@@ -41,6 +75,9 @@ public class Joueur implements Serializable {
         return Question.saisir(reponse);
     }
 
+    /**
+     * Afficher.
+     */
     public void afficher() {
         System.out.println("Joueur :");
         System.out.println("N° " + numero);
@@ -49,6 +86,18 @@ public class Joueur implements Serializable {
         System.out.println("Etat  : " + this.etat);
     }
 
+    /**
+     * Gets etat.
+     *
+     * @return the etat
+     */
+    public String getEtat() {
+        return etat;
+    }
+
+    /**
+     * Changer etat.
+     */
     public void changerEtat() {
         Scanner sc = new Scanner(System.in);
         System.out.println("Saisissez l'\u00e9tat :");
@@ -59,10 +108,30 @@ public class Joueur implements Serializable {
         this.etat = etat;
     }
 
+    /**
+     * Gets numero.
+     *
+     * @return the numero
+     */
+    public int getNumero() {
+        return numero;
+    }
+
+
+    /**
+     * Sets score.
+     *
+     * @param score the score
+     */
     public void setScore(int score) {
         this.score += score;
     }
 
+    /**
+     * Gets score.
+     *
+     * @return the score
+     */
     public int getScore() {
         return score;
     }
